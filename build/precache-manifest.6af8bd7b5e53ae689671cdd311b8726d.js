cf85419a971f654906280553befb0c385deb904f
Sergei Sevriugin <sevriugin@gmail.com>
2020-08-29 17:45:13 +0200

    build added

---
+++ build/precache-manifest.6af8bd7b5e53ae689671cdd311b8726d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7745fe668f75852c4470558b73d2da2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d07066bf31b1ff6a1dc0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ddcf60fb7daf9362ab87",
+    "url": "/static/js/2.fe527716.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe527716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d07066bf31b1ff6a1dc0",
+    "url": "/static/js/main.8e8708bd.chunk.js"
+  },
+  {
+    "revision": "8b5073a67111caf0ce04",
+    "url": "/static/js/runtime-main.46ced658.js"
+  }
+]);
